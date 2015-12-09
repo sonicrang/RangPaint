af1191a8da56b088e5f67708296466289024a24b
wurang <sonicrang@gmail.com>
2015-12-09 11:50:26 +0800

    commit resize thumb

---
+++ RangPaint/Controls/ResizeThumb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace RangPaint.Controls
+{
+    public class ResizeThumb : Thumb
+    {
+        public ResizeThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.ResizeThumb_DragDelta);
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var thumb = sender as ResizeThumb;
+            if (thumb != null)
+            {
+                Grid item = thumb.Parent as Grid;
+
+                item.Height = item.ActualHeight + (int)e.VerticalChange;
+                item.Width = item.ActualWidth + (int)e.HorizontalChange;
+            }
+
+            e.Handled = true;
+        }
+    }
+}
